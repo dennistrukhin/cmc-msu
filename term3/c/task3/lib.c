f1c29b2e8ea7258b9576b6467c07c4043ef63db9
Dennis Trukhin <dennis.trukhin@gmail.com>
2017-12-07 13:29:32 +0300

    Task 3: lib.c + cmp

---
+++ term3/c/task3/lib.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <memory.h>
+
+char * concat(const char *s1, const char *s2)
+{
+    char * result = malloc(strlen(s1) + strlen(s2) + 1);
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
+char * get_current_directory()
+{
+    char * cwd;
+    cwd = getcwd(0, 0);
+    return cwd;
+}
+
+char * abs_path(char * path)
+{
+    if (*path != '/') {
+        return concat(concat(get_current_directory(), "/"), path);
+    }
+    return path;
+}
---
+++ term3/c/task3/lib.h
@@ -0,0 +1,10 @@
+#ifndef UNIX_CMD_LIB_H
+#define UNIX_CMD_LIB_H
+
+#endif
+
+char * get_current_directory();
+char * abs_path(char * path);
+char * concat(const char *s1, const char *s2);
+
+
