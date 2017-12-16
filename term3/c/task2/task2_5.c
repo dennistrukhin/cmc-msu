20af6c26eddc22d5bebf7a854a6b207b2fef3764
Dennis Trukhin <dennis.trukhin@gmail.com>
2017-12-16 22:49:36 +0300

    task 2:5 fixes

--- term3/c/task2/task2_5.c
+++ term3/c/task2/task2_5.c
@@ -7,40 +7,40 @@ typedef struct Node {
     struct Node *next;
 } Node;
 
-Node *head = NULL;
-int first_output = 1;
-
-void append(Node **head, char *str) {
+void append(Node ** head, char * str) {
     // Тут мы создали новый элемент в списке. Пока что он просто "болтается" в памяти
-    Node *tmp = (Node*) malloc(sizeof(Node));
-    char *new_str = (char*) malloc(128);
+    Node * tmp = (Node *) malloc(sizeof(Node));
+    char * new_str = (char *) malloc(128);
     strncpy(new_str, str, 128);
     tmp->str = new_str;
     tmp->next = NULL;
     // Теперь надо понять, кто будет на него указывать
-    Node **ptr = head;
-    while (*ptr != NULL) {
+    Node ** ptr = head;
+    while (* ptr != NULL) {
         ptr = &((*ptr)->next);
     }
-    (*ptr) = tmp;
+    *ptr = tmp;
 }
 
 
 int main() {
-    char *str;
-    Node **ptr;
+    char * str;
+    Node * head = NULL;
+    Node ** ptr;
+    int first_output = 1;
+    fprintf(stdout, "==>\n");
     while (scanf("%s", str) != EOF) {
         append(&head, str);
     }
     // В str у нас остался указатель на последнюю строку, так что теперь можно перебрать список
-    // и удалить те эелементы, у которых строка такая же и при этом указатена следующий элемент не NULL
+    // и удалить те эелементы, у которых строка такая же и при этом указатель на следующий элемент не NULL
     // Пройдёмся по списку
     ptr = &head;
     while (*ptr != NULL) {
         if (strcmp(str, (*ptr)->str) == 0 && (*ptr)->next != NULL) {
-            (*ptr) = (*ptr)->next;
+            *ptr = (*ptr)->next;
         } else {
-            ptr = &((*ptr)->next);
+            ptr = &(*ptr)->next;
         }
     }
 
