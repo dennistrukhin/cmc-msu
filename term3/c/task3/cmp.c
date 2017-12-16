810d735f49a5a9820d82a02a7e832891dae59b94
Dennis Trukhin <dennis.trukhin@gmail.com>
2017-12-16 20:01:17 +0300

    bugfixes in echo and cmp

--- term3/c/task3/cmp.c
+++ term3/c/task3/cmp.c
@@ -3,14 +3,35 @@
 #include <stdlib.h>
 #include "lib.h"
 
-void compare(char * path1, char * path2)
+int main(int argc, char * argv[])
 {
+    char * path1;
+    char * path2;
+
     FILE * f1, * f2;
     ssize_t read1, read2;
-    char * line1, * line2;
-    size_t len1, len2;
+    char * line1 = NULL, * line2 = NULL;
+    size_t len1 = 0, len2 = 0;
     int row = 0, col = 0;
-    fprintf(stdout, "Comparing %s with %s\n", path1, path2);
+
+    if (argc < 3)
+    {
+        fprintf(stderr, "cmp: missing operand after 'cmp'");
+        return  1;
+    }
+    path1 = abs_path(argv[1]);
+    path2 = abs_path(argv[2]);
+    if (access(path1, F_OK) == -1)
+    {
+        fprintf(stderr, "cmp: %s: No such file or directory\n", argv[1]);
+        return 1;
+    }
+    if (access(path2, F_OK) == -1)
+    {
+        fprintf(stderr, "cmp: %s: No such file or directory\n", argv[2]);
+        return 1;
+    }
+
     f1 = fopen(path1, "r");
     if (f1 == NULL) {
         fprintf(stderr, "Error opening %s", path1);
@@ -28,39 +49,15 @@ void compare(char * path1, char * path2)
             break;
         }
         ++row; // Считаем строки с 1, а не с 0, так как в редакторах/IDE они пронумерованы с 1, так удобнее
+        col = 0;
         while (col < len1 && col < len2 && (line1[col] != '\n' || line2[col] != '\n'))
         {
             if (line1[col] != line2[col])
             {
-                fprintf(stdout, "First diff is in row %d col %d\n", row, col);
+                fprintf(stdout, "%s %s differ: char %d, line %d\n", argv[1], argv[2], (col + 1), row);
                 exit(0);
             }
             ++col;
         }
     }
-    fprintf(stdout, "Files are identical\n");
-}
-
-int main(int argc, char * argv[])
-{
-    char * path1;
-    char * path2;
-    if (argc < 3)
-    {
-        fprintf(stderr, "Paths to two files should be provided");
-        return  1;
-    }
-    path1 = abs_path(argv[1]);
-    path2 = abs_path(argv[2]);
-    if (access(path1, F_OK) == -1)
-    {
-        fprintf(stderr, "First file does not exist");
-        return 1;
-    }
-    if (access(path2, F_OK) == -1)
-    {
-        fprintf(stderr, "Second file does not exist");
-        return 1;
-    }
-    compare(path1, path2);
 }
--- term3/c/task3/echo.c
+++ term3/c/task3/echo.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
+#include <memory.h>
 
 const char * WITH_SPACE = "%s ";
-const char * WITH_NEW_LINE = "%s\n";
+const char * WITHOUT_SPACE = "%s";
 
 int main(int argc, char * argv[])
 {
     int i = 0;
-    for (i = 1; i < argc; i++)
+    int print_last_new_line;
+    print_last_new_line = (argc > 1 && strcmp(argv[1], "-n") == 0) ? 0 : 1;
+    for (i = 2 - print_last_new_line; i < argc; i++)
     {
-        fprintf(stdout, i == argc ? WITH_NEW_LINE : WITH_SPACE, argv[i]);
+        fprintf(stdout, i == argc - 1 ? WITHOUT_SPACE : WITH_SPACE, argv[i]);
+    }
+    if (print_last_new_line) {
+        fprintf(stdout, "\n");
     }
 }
